a41c4e51da5db3296a6779f1ccf6dd4147f44b7a
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-02 12:34:19 -0300

    Commit dos códigos da Intro. a Programação com C#

---
+++ Introdução a Programação com C#/03-QuantaMandioca/03-QuantaMandioca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03_QuantaMandioca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chico = 300 * Int32.Parse(Console.ReadLine());
+            double bento = 1500 * Int32.Parse(Console.ReadLine());
+            double bernardo = 600 * Int32.Parse(Console.ReadLine());
+            double marina = 1000 * Int32.Parse(Console.ReadLine());
+            double iara = 150 * Int32.Parse(Console.ReadLine());
+            double marlene = 225;
+            double total = 0; // Digite aqui o calculo total
+            total = chico + bento + bernardo + marina + iara + marlene;
+            Console.WriteLine(total);
+        }
+    }
+}
