9170ca7927435cda83b276e820a02a15b809958f
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-02 12:18:50 -0300

    Códigos da introdução a programação com .Net

---
+++ Introdução a Programação com .NET/03-CoxinhaDeBueno/03-CoxinhaDeBueno/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03_CoxinhaDeBueno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(" ");
+            double a = double.Parse(line[0]);
+            double b = double.Parse(line[1]);
+
+            double result = a / b;
+
+            Console.WriteLine(result.ToString("0.00"));
+
+        }
+    }
+}
