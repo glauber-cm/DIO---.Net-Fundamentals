a65cfafaaf77240b07805f03b45b4fda2ae4a204
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-03 10:56:03 -0300

    Add files via upload

---
+++ Solução de problemas com .NET/04-ValidadorSenhaComRequisitos/ValidadorSenhaComRequisitos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidadorSenhaComRequisitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha;
+
+            while (!string.IsNullOrEmpty(senha = Console.ReadLine()))
+            {
+                var contLetraMaiuscula = Regex.Matches(senha, "[A-Z]").Count;
+                var contLetraMinuscula = Regex.Matches(senha, "[a-z]").Count;
+                var contNumero = Regex.Matches(senha, "[0-9]").Count;
+
+                if (senha.Length >= 6
+                  && senha.Length <= 32
+                  && contLetraMaiuscula > 0
+                  && contLetraMinuscula > 0
+                  && contNumero > 0
+                  && (contLetraMaiuscula + contLetraMinuscula + contNumero == senha.Length))
+                {
+                    Console.WriteLine("Senha valida.");
+                }
+                else
+                {
+                    Console.WriteLine("Senha invalida.");
+                }
+            }
+
+        }
+    }
+}
