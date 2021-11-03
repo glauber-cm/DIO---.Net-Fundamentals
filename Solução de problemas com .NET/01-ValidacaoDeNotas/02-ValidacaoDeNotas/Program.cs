8fe7a9df6eadf827be8d63da281c2bfa19485c4f
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-03 10:54:28 -0300

    Commit dos códigos da Sol. problemas em .NET

---
+++ Solução de problemas com .NET/01-ValidacaoDeNotas/02-ValidacaoDeNotas/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _02_ValidacaoDeNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare suas variáveis corretamente
+            int opcao = 1;
+            double nota1 = 0;
+            double nota2 = 0;
+            bool validador = false;
+            string entrada;
+            double media = 0;
+
+            while (opcao != 2)
+            {
+                while (validador == false)
+                {
+                    entrada = Console.ReadLine();
+                    if (double.Parse(entrada) >= 0 && double.Parse(entrada) <= 10)
+                    {
+                        nota1 = double.Parse(entrada);
+                        validador = true;
+                    }
+                    else
+                        Console.WriteLine("nota invalida");
+                }
+
+                validador = false;
+
+                while (validador == false)
+                {
+                    entrada = Console.ReadLine();
+                    if (double.Parse(entrada) >= 0 && double.Parse(entrada) <= 10)
+                    {
+                        nota2 = double.Parse(entrada);
+                        validador = true;
+                    }
+                    else
+                        Console.WriteLine("nota invalida");
+
+                }
+
+                media = (nota1 + nota2) / 2;
+                Console.WriteLine($"media = {media.ToString("N2")}");
+
+                validador = false;
+
+                while (validador == false)
+                {
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    entrada = Console.ReadLine();
+                    opcao = int.Parse(entrada);
+                    if (opcao == 1 || opcao == 2)
+                    {
+                        validador = true;
+                    }
+                }
+
+                validador = false;
+            }
+        }
+    }
+}
+
+
+
+
