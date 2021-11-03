49711bc842a8e208ac98fa0669b28687abf7852a
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2021-11-03 10:57:23 -0300

    Commit dos códigos da Sol. problemas em .NET

---
+++ Solução de problemas com .NET/05-FilaDoBanco/FilaDoBanco/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilaDoBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeTestes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numeroDeTestes; i++)
+            {
+                if (i <= 1000)
+                {
+                    int clientes = int.Parse(Console.ReadLine());
+
+                    List<int> lista = new List<int>();
+                    string[] linhaNumeroSMS = Console.ReadLine().Split(" ");
+
+                    foreach (var item in linhaNumeroSMS)
+                    {
+                        if (lista.Count <= 1000)
+                        {
+                            lista.Add(int.Parse(item));
+                        }
+                    }
+
+                    lista.Sort((a, b) => b.CompareTo(a));
+                    int qtd = clientes;
+
+                    for (int x = 0; x < lista.Count; x++)
+                    {
+                        if (int.Parse(linhaNumeroSMS[x]) != lista[x])
+                        {
+                            qtd--;
+                        }
+                    }
+                    Console.WriteLine(qtd);
+                }
+            }
+        }
+    }
+}
